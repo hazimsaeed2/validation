5868fbda150e2825e79e6414f3ea258f4052492a
rbisa <rbisa@bjs.com>
2023-09-13 13:00:06 -0400

    Adding print

--- assignment/scripts/input_mbr_generation.py
+++ assignment/scripts/input_mbr_generation.py
@@ -34,11 +34,9 @@ Output_path = args.Output_path
 mbr =spark.read.csv(Rec_file_path, header = True).withColumnRenamed('BBM Decile (Member DNA)', 'DECILE').withColumnRenamed('BBM Score (Member DNA)', 'score')
 DNA = spark.read.parquet(DNA_path).select('mbrshp_sid', 'cpn_channel')
 
-print("mbr.count()")
-mbr.count()
+print(f"mbr.count() : {mbr.count()}")
 
-print("mbr.dtypes")
-mbr.dtypes
+print(f"mbr.dtypes : {mbr.dtypes}")
 
 print("mbr.show(5)")
 mbr.show(5)
@@ -56,52 +54,44 @@ mbr.createOrReplaceTempView('mbr')
 
 mbr = mbr.withColumn("FHH_IND", lit('N')) #Engine doesn't use this column but still call this column
 
-print("check if duplicate from Scott is correct or not")
-mbr.select('MBRSHP_NBR').dropDuplicates().count()
+print(f"check if duplicate from Scott is correct or not :{mbr.select('MBRSHP_NBR').dropDuplicates().count() }")
 
-print("check after dropping na and duplicates")
-mbr.select('MBRSHP_NBR').dropna().dropDuplicates().count()
+print(f"check after dropping na and duplicates : {mbr.select('MBRSHP_NBR').dropna().dropDuplicates().count()}")
 
 mbr = mbr.dropna(subset='MBRSHP_NBR')
 
-print("check count after dropping na and duplicates in mbr")
-mbr.count()
+print(f"check count after dropping na and duplicates in mbr : {mbr.count()}")
 
 #purge(LONGINITIDUNAL TEST)
 BBM11_purge= spark.read.csv(BBM11_purge_path, header = True )#.select('MBRSHP_SID', 'MBRSHP_NBR')
 
 mbr.join(BBM11_purge, 'MBRSHP_NBR').count() #CHECK IF 0, LONGITIDINAL COUNT SHOULD BE == 0
 
-print("BBM11_purge.count()")
-BBM11_purge.count()
+print(f"BBM11_purge.count() : {BBM11_purge.count()}")
 
 print("left_anti ==> CHECK IF  count == mbr input")
-mbr.join(BBM11_purge, 'MBRSHP_NBR', 'LEFT_ANTI').count()
+print(mbr.join(BBM11_purge, 'MBRSHP_NBR', 'LEFT_ANTI').count())
 
-print("mbr count after joining left_anti with BBM11_purge")
-mbr.count()
+print(f"mbr count after joining left_anti with BBM11_purge : {mbr.count()}")
 
 mbr.dropDuplicates()
 
-print("mbr count after dropping duplicates")
-mbr.count()
+print(f"mbr count after dropping duplicates : {mbr.count()}")
 
 print("mbr.groupBy('DECILE').count().show()")
 mbr.groupBy('DECILE').count().show()
 
 mbr_filtered = mbr.join(DNA, "MBRSHP_SID").filter(col("cpn_channel").isin("paper","dual","digital")).filter(col("DECILE").isin(1,2,3,4))
 
-print("mbr filtered for P&D cpn_channel")
-mbr_filtered.count()
+print(f"mbr filtered for P&D cpn_channel : {mbr_filtered.count()}")
 
 print("mbr by cpn_channel")
-mbr_filtered.groupBy("cpn_channel").count().show()
+print(mbr_filtered.groupBy("cpn_channel").count().show())
 
 print("mbr by decile")
-mbr_filtered.groupBy("DECILE").count().show()
+print(mbr_filtered.groupBy("DECILE").count().show())
 
-print("mbr count")
-mbr.count()
+print(f"mbr count : {mbr.count()}")
 
 print(f"writing to output path : {Output_path}")
 mbr.dropDuplicates().repartition(10).write.option("header", "true").mode("overwrite").csv(Output_path)
